620c5c815c8717d1a6779cf71e61fea96ed84b86
Jason B <jasonbetsargon@gmail.com>
2023-11-05 20:31:15 -0800

    initial file setup

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "raylib.h"
+
+int main()
+{
+    // array of window dimensions
+    int windowDimensions[2];
+    windowDimensions[0]=384;
+    windowDimensions[1]=384;
+
+    // initialize window
+    InitWindow(windowDimensions[0],windowDimensions[1],"TOP-DOWN");
+
+    // load map texture
+    
+}
