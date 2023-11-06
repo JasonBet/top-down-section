1f186f2b338c046164f458c22ae4ef3eb93616dc
Jason B <jasonbetsargon@gmail.com>
2023-11-05 20:37:25 -0800

    finalizing startup file

--- main.cpp
+++ main.cpp
@@ -11,5 +11,24 @@ int main()
     InitWindow(windowDimensions[0],windowDimensions[1],"TOP-DOWN");
 
     // load map texture
-    
+    Texture2D WorldMap=LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2 mapPos{0.0,0.0};
+
+    // game loop
+    while(!WindowShouldClose())
+    {
+        // start drawing
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        DrawTextureEx(WorldMap,mapPos,0.0,4,WHITE);
+
+
+
+        EndDrawing();
+    }
+
+    UnloadTexture(WorldMap);
+
+    CloseWindow();
 }
