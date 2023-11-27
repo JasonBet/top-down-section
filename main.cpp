c3df6391e8a515594ee36029a00d7bbbdbd9b7c0
Jason B <jasonbetsargon@gmail.com>
2023-11-26 20:43:19 -0800

    created prop system & added 2 props

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "Character.h"
+#include "Prop.h"
 
 int main()
 {
@@ -19,6 +20,11 @@ int main()
 
     Character knight{windowDimensions[0],windowDimensions[1]};
 
+    Prop props[2]{
+        Prop{Vector2{600.f,300.f}, LoadTexture("nature_tileset/Rock.png")},
+        Prop{Vector2{400.f,500.f}, LoadTexture("nature_tileset/Log.png")}
+    };
+
     SetTargetFPS(60);
     // game loop
     while (!WindowShouldClose())
@@ -30,6 +36,11 @@ int main()
         mapPos=Vector2Scale(knight.getWorldPos(),-1.f);
         // draw the map
         DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
+
+        for(auto prop: props)
+        {
+            prop.Render(knight.getWorldPos());
+        }
         knight.tick(GetFrameTime());
         // check map bounds
         if(knight.getWorldPos().x<0.f ||
