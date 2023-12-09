9ae31eb1628fdb30884c45d7f8abacb45f26bff9
Jason B <jasonbetsargon@gmail.com>
2023-12-09 12:58:21 -0800

    corrected Wreorder warning to improve efficiency

--- Prop.h
+++ Prop.h
@@ -7,7 +7,7 @@ public:
     void Render(Vector2 knightPos);
     Rectangle GetCollisionRec(Vector2 knightPos);
 private:
-    Texture2D texture{};
     Vector2 worldPos{};
+    Texture2D texture{};
     float scale{4.f};
 };
